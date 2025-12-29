08f8efe9c3c067f9cabb34218847ec197d10cccc
Akash Bhalodiya <akash.bhalodiya@tatvasoft.com>
2025-12-29 18:15:13 +0530

    update baseentity model to use encrypted id

--- Inventory.Models/Entities/BaseEntity.cs
+++ Inventory.Models/Entities/BaseEntity.cs
@@ -1,9 +1,22 @@
+using System.Text.Json.Serialization;
+using Inventory.Common.Helpers;
+
 namespace Inventory.Models.Entities;
 
 public abstract class BaseEntity
 {
+    [JsonIgnore]
     public int? CreatedBy { get; set; }
+    public string? EncryptedCreatedBy
+    {
+        get => EncryptionHelper.EncryptId(CreatedBy.ToString());
+    }
+    [JsonIgnore]
     public int? ModifiedBy { get; set; }
+    public string? EncryptedModifiedBy
+    {
+        get => EncryptionHelper.EncryptId(ModifiedBy.ToString());
+    }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? ModifiedAt { get; set; } = DateTime.UtcNow;
 }
